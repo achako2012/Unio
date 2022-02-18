bf0f046ccbe77d3ae970b050032bb8d6c977915c
achako2012 <a.chako2012@gmail.com>
2022-02-18 14:52:15 +0100

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,28 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 17,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        750: {
+            slidesPerView: 3
+        },
+        1100: {
+            slidesPerView: 4
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+});
